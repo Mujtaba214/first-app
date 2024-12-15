6701c5bb7bf9b4af5e6017ab623fd7252b4db4c1
Mujtaba214 <142329119+Mujtaba214@users.noreply.github.com>
2024-12-15 13:39:18 +0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import ReactDom from "react-dom"
+
+let root = document.getElementById("root")
+
+ReactDom.createRoot(root).render(
+  <div>
+    <h1 style={{fontSize:"50px"}}>Hey I'm in react 1</h1>
+    <p style={{ fontSize: "20px" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi similique magnam corporis possimus mollitia incidunt aliquam aperiam nemo doloremque, quidem sint laboriosam ex voluptatem excepturi velit laborum voluptatibus. Rerum doloremque odit nesciunt enim aspernatur at neque vitae hic aperiam laborum iste illo expedita suscipit dolorem dolorum maxime, temporibus laudantium nisi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis blanditiis atque non impedit laudantium laboriosam autem distinctio animi nostrum obcaecati unde neque voluptatem, earum amet dignissimos molestias? A tempore vel reiciendis illum odio est enim fugit placeat natus tempora!</p>
+    <img style={{width:"10%"}} src="/public/vite.svg" alt="" />
+    <h1 style={{ fontSize: "50px" }}>Hey I'm in react 2</h1>
+    <p style={{ fontSize: "20px" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi similique magnam corporis possimus mollitia incidunt aliquam aperiam nemo doloremque, quidem sint laboriosam ex voluptatem excepturi velit laborum voluptatibus. Rerum doloremque odit nesciunt enim aspernatur at neque vitae hic aperiam laborum iste illo expedita suscipit dolorem dolorum maxime, temporibus laudantium nisi. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt repudiandae consectetur sunt nisi provident at cum numquam optio quidem. Cum totam enim ducimus hic deserunt provident nam, voluptates quae amet maxime, nemo accusamus similique id facere eos eveniet, numquam fugit reprehenderit fuga! Incidunt minus eligendi tempora? Sapiente qui reprehenderit distinctio.</p>
+    <img style={{width:"10%"}} src="/src/assets/react.svg" alt="" />
+    <h1 style={{fontSize:"50px"}}>Hey I'm in react 3</h1>
+    <p style={{fontSize:"20px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi similique magnam corporis possimus mollitia incidunt aliquam aperiam nemo doloremque, quidem sint laboriosam ex voluptatem excepturi velit laborum voluptatibus. Rerum doloremque odit nesciunt enim aspernatur at neque vitae hic aperiam laborum iste illo expedita suscipit dolorem dolorum maxime, temporibus laudantium nisi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis blanditiis atque non impedit laudantium laboriosam autem distinctio animi nostrum obcaecati unde neque voluptatem, earum amet dignissimos molestias? A tempore vel reiciendis illum odio est enim fugit placeat natus tempora!</p>
+    <img style={{width:"10%"}} src="/public/vite.svg" alt="" />
+    <h1 style={{fontSize:"50px"}}>Hey I'm in react 4</h1>
+    <p style={{fontSize:"20px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi similique magnam corporis possimus mollitia incidunt aliquam aperiam nemo doloremque, quidem sint laboriosam ex voluptatem excepturi velit laborum voluptatibus. Rerum doloremque odit nesciunt enim aspernatur at neque vitae hic aperiam laborum iste illo expedita suscipit dolorem dolorum maxime, temporibus laudantium nisi. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt repudiandae consectetur sunt nisi provident at cum numquam optio quidem. Cum totam enim ducimus hic deserunt provident nam, voluptates quae amet maxime, nemo accusamus similique id facere eos eveniet, numquam fugit reprehenderit fuga! Incidunt minus eligendi tempora? Sapiente qui reprehenderit distinctio.</p>
+    <img style={{width:"10%"}} src="/src/assets/react.svg" alt="" />
+  </div>
+)
